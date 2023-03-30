fc209b192261be6d4a26a39253886e3937978045
jacobboykin <boykinmusic@gmail.com>
2023-03-29 23:39:16 -0400

    fix: add Service RBAC

--- controllers/swanson_controller.go
+++ controllers/swanson_controller.go
@@ -58,6 +58,7 @@ type SwansonReconciler struct {
 //+kubebuilder:rbac:groups=core,resources=events,verbs=create;patch
 //+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch
+//+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
